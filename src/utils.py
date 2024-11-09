f7422875aa99b0afcd83ddb87e13aa91534e3b28
Erfan Khalaji <erfan.khalaji@protonmail.com>
2024-11-08 23:32:14 -0700

    update
    add proper file structure

--- src/utils.py
+++ src/utils.py
@@ -2,27 +2,26 @@ import os
 import yaml
 
 
-
-
-
 def get_apikey():
     """
     Reads API key from a configuration file.
 
-    This function opens a configuration file named "apikeys.yml", reads the 
-    API key for OpenAI
+    This function opens a configuration file named "apikeys.yml", reads the API key for OpenAI
 
-    Returns: api_key (str): The OpenAI API key.
+    Returns:
+    api_key (str): The OpenAI API key.
     """
     
     # Construct the full path to the configuration file
-    config_dir = "config/"
-    file_path = os.path.join(config_dir, "apikeys.yml")
+    script_dir = "/usercode/"
+    file_path = os.path.join(script_dir, "apikeys.yml")
+
     with open(file_path, 'r') as yamlfile:
         loaded_yamlfile = yaml.safe_load(yamlfile)
         API_KEY = loaded_yamlfile['openai']['api_key']
-    
-    print(API_KEY)
+        
     return API_KEY
 
-get_apikey()
+
+if __name__ == "__main__":
+    print("API_KEY", get_apikey())
